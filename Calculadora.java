1cc2fc6bef519c945fafd38e55c1354b8d2d0912
leo-gc <105380016+leo-gc@users.noreply.github.com>
2023-02-23 09:05:53 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,31 @@
+public class Calculadora {
+    //Soma
+    public float somar(float a, float b) {
+        return a + b;
+    }
+
+    //Subtração
+    public float subtrair(float a, float b) {
+        return a - b;
+    }
+
+    //Multiplicação
+    public float multiplicar(float a, float b) {
+        return a * b;
+    }
+
+    //Divisão
+    public float dividir(float a, float b) {
+        return a / b;
+    }
+
+    //Potenciação
+    public float potencia(float a, float b) {
+        return a ** b;
+    }
+
+    //Radiciação
+    public float raiz(float a) {
+        return Math.sqrt(a);
+    }
+}
